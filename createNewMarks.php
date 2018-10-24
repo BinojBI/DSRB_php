f8e222194d569546d2b50590a3f3540a2abd2371
BinojBI <binojishara21@gmail.com>
2018-10-25 00:21:36 +0530

    add new pages modified marks and statics page

---
+++ createNewMarks.php
@@ -0,0 +1,73 @@
+<?php require 'cdn.php';
+require 'db/connection.php';
+ ?>
+ <?php
+ //get student details
+  //$query = "SELECT firstName FROM student EXCEPT SELECT studentName FROM marks";
+ $query = "SELECT firstName,lastName FROM student";
+ $result_set = mysqli_query($connection,$query);
+ $numOfRow = mysqli_num_rows($result_set);
+
+
+//check $query get marks table
+//$queryMsrkSelect = "SELECT studentName FROM marks";
+// $result_setqms = mysqli_query($connection,$queryMsrkSelect);
+// $numOfRow = mysqli_num_rows($result_setqms);
+
+
+   //insert new marks
+ if(isset($_POST['submit'])){
+   $studentName = $_POST['studentName'];
+   $maths = $_POST['maths'];
+   $chemistry = $_POST['chemistry'];
+   $physics = $_POST['physics'];
+   $total = $maths + $physics + $chemistry;
+   $queryPost = "INSERT INTO marks(studentName,maths,chemistry,physics,total) VALUES ('{$studentName}',{$maths},{$chemistry},{$physics},{$total})";
+   $result = mysqli_query($connection, $queryPost);
+
+   if($result){
+     echo "1 record added";
+   }else{
+     echo "databse query failed";
+    echo (mysqli_error($connection));
+   }
+ }
+  ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Add marks</title>
+   </head>
+   <body>
+     <h2>Add marks</h2>
+     <form action="createNewMarks.php" method="post">
+       <label class="mr-sm-2" for="inlineFormCustomSelect">Student</label>
+
+       <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="studentName">
+           <?php
+           echo '<option selected>Choose...</option>';
+           for ($i=1; $i<= $numOfRow ; $i++) {
+             $record = mysqli_fetch_assoc($result_set);
+
+             $id = $record['id'];
+             $firstName = $record['firstName'];
+             $lastName = $record['lastName'];
+
+             echo '
+             <option value="'.$firstName.'">'.$firstName .' '. $lastName.'</option>';
+           }
+            ?>
+
+             </select>
+            <input type="text" placeholder="Maths" name="maths"><br>
+            <input type="text" placeholder="Chemistry" name="chemistry"><br>
+            <input type="text" placeholder="Physics" name="physics"><br>
+
+            <input type="submit" name="submit" value="submit">
+
+
+     </form>
+
+   </body>
+ </html>
--- marks.php
+++ marks.php
@@ -1,1 +1,57 @@
-<?php require 'header.php'; ?>
+<?php require 'header.php';
+require 'db/connection.php';
+ ?>
+ <?php
+ $query = "SELECT * FROM marks";
+ $result_set = mysqli_query($connection,$query);
+ $numOfRow = mysqli_num_rows($result_set);
+ //echo $numOfRow;
+
+  ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Marks Page</title>
+   </head>
+   <body>
+<a href="createNewMarks.php">Create new</a><br><br>
+<table class="table">
+<thead>
+<tr>
+  <th scope="col">Id</th>
+  <th scope="col">First Name</th>
+  <th scope="col">Maths</th>
+  <th scope="col">Chemistry</th>
+  <th scope="col">Physics</th>
+  <th scope="col">#</th>
+
+</tr>
+</thead>
+<tbody>
+<?php
+
+for ($i=1; $i<= $numOfRow ; $i++) {
+  $record = mysqli_fetch_assoc($result_set);
+
+  $id = $record['id'];
+  $studentName = $record['studentName'];
+  $maths = $record['maths'];
+  $chemistry = $record['chemistry'];
+  $physics = $record['physics'];
+
+
+echo '<tr>
+    <td>'. $id . '</td>
+    <td>'. $studentName .'</td>
+    <td>'. $maths.'</td>
+    <td>'.  $chemistry .'</td>
+    <td>'. $physics .'</td>
+    <td><a href="editMarks.php">edit</a> | <a href="deleteMarks.php">delete</a> </td>
+  </tr>';
+} ?>
+
+</tbody>
+</table>
+   </body>
+ </html>
