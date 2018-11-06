56ec8de3d5d463727bafa16d0ef3690d3ef5cf77
BinojBI <binojishara21@gmail.com>
2018-11-07 02:16:29 +0530

    addded footer file and designed UI

---
+++ footer.php
@@ -0,0 +1,51 @@
+<!-- Footer -->
+<footer class="page-footer font-small" style="background-color:rgb(170, 102, 204);">
+
+    <!-- Footer Links -->
+    <div class="container">
+
+      <!-- Grid row-->
+      <div class="row text-center d-flex justify-content-center pt-5 mb-3">
+
+        <!-- Grid column -->
+        <div class="col-md-2 mb-1">
+          <h6 class="text-uppercase font-weight-bold">
+            <a href="#!">About us</a>
+          </h6>
+        </div>
+        <!-- Grid column -->
+
+
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+      <hr class="rgba-white-light" style="margin: 0;">
+
+      <!-- Grid row-->
+      <div class="row d-flex text-center justify-content-center mb-md-0 mb-1">
+
+        <!-- Grid column -->
+        <div class="col-md-8 col-12 mt-3">
+          <p style="line-height: 1.7rem">This project is MIS group work who involving all three students & completed</p>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row-->
+      <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+
+
+      <!-- Grid row-->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/education/bootstrap/"> MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
